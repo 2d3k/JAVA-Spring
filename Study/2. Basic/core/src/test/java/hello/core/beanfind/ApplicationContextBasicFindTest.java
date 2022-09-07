c8a8b12a506dac9b0df19772fa6467fad272c1bf
2D3 <as13337@gmail.com>
2022-09-07 18:40:16 +0900

    Section 4

---
+++ Study/2. Basic/core/src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,51 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+//스프링 빈 조회 (기본)
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByType2() {
+        MemberServiceImpl memberService = ac.getBean(MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanByNameX() {
+        //MemberService xxxxx = ac.getBean("XXXXX", MemberService.class);
+
+        //NoSuchBeanDefinitionException: 예외가 발생해야 테스트가 성공함
+        //(2)의 로직을 실행했을 때 (1)의 예외가 발생함
+        assertThrows(NoSuchBeanDefinitionException.class,                  //(1)
+                () -> ac.getBean("XXXXX", MemberService.class));    //(2)
+    }
+}
